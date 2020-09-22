4aa37c6b1deb8343f7626cb9010e98e9391ae33d
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-22 18:39:41 -0400

    revised App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper'
 import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
-      <Search />
+      <Header />
+      <Wrapper>
+        <Search></Search>
+      </Wrapper>
+      <Footer />
     </div>
   );
 }
