300614a8b907a1cb3069c0018d89ba68d0d09752
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-22 19:35:20 -0400

    revised sort

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,15 +1,19 @@
-function checkLastName(a,b) {
-    let nameA = a.name.last.toUpperCase();
-    let nameB = b.name.last.toUpperCase();
+function checkSort(a,b) {
+    let nameA = a.toUpperCase();
+    let nameB = b.toUpperCase();
     if (nameA < nameB) return -1;
     if (nameA > nameB) return 1;
     return 0;
 }
 
 export function sortNames(arr) {
-    return arr.sort(checkLastName);
+    return arr.sort((a, b) => checkSort(a.name.last, b.name.last));
 }
 
 export function sortNumbers(arr) {
     return arr.sort();
+}
+
+export function sortEmails(arr) {
+    return arr.sort((a,b) => checkSort(a.email, b.email))
 }
