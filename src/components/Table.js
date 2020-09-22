06e76ea988a4826d7ed49329cbf62549c9b1b969
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-22 13:57:05 -0400

    fixed table header

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import EmployeeRow from './EmployeeRow';
 import getUsers from '../utils/API'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Table() {
     const employees = getUsers();
     return (
-        <div>
-        <table>
-            <thead className="table table-dark">
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Picture</th>
-            </thead>
-            <tbody>
-                {employees.map(item => <EmployeeRow employee={item}/>)}
-            </tbody>
-        </table>
+        <div className='container-fluid mx-auto justify-content-center'>
+            <table className="table table-striped">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone Number</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map(item => <EmployeeRow employee={item} />)}
+                </tbody>
+            </table>
         </div>
     )
 }
