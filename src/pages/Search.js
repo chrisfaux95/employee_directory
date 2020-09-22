f654ee3f4232781fb14d947d96dfa28fe9043772
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-22 13:56:44 -0400

    created Search page

---
+++ src/pages/Search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Table from '../components/Table';
+
+export default function Search() {
+    return (
+        <main>
+            <Header />
+            <Table />
+            <Footer />
+        </main>
+    )
+}
