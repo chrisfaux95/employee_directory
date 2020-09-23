12ba6eba3c57ce3cb0d68369598e212a381be21d
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-23 11:18:23 -0400

    revised table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import EmployeeRow from './EmployeeRow';
 import getUsers from '../utils/API'
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { sortNames, sortEmails } from '../utils/sort';
 
 export default function Table() {
     const employees = getUsers();
+    const [employeesSorted, setEmployeesSorted] = useState(employees);
+
+    // const handleInputChange = () => {
+
+    // }
+
+    const handleNameSort = () => {
+        console.log('sort names')
+        sortNames(employeesSorted)
+        setEmployeesSorted(employeesSorted);
+        console.log(employeesSorted)
+    }
+
+    const handleEmailSort = () => {
+        console.log('sort emails')
+        sortEmails(employeesSorted);
+        setEmployeesSorted(employeesSorted);
+    }
+
     return (
         <div className='container-fluid mx-auto justify-content-center'>
+            <input type="text" id="employee"></input>
             <table className="table table-striped">
                 <thead className="thead-dark">
                     <tr>
                         <th scope="col"></th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
+                        <th scope="col">Name&nbsp;
+                        <i onClick={handleNameSort}
+                        className="fas fa-sort"> 
+                                </i>
+                        </th>
+                        <th scope="col">Email&nbsp;
+                        <i onClick={handleEmailSort}
+                                className="fas fa-sort"></i>
+                        </th>
                         <th scope="col">Phone Number</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {employees.map(item => <EmployeeRow employee={item} />)}
+                    {employeesSorted.map(item => <EmployeeRow employee={item} />)}
                 </tbody>
             </table>
         </div>
